08290a57db6a17c8200e39fa0bab9699dcd74bc0
Oli <oli@olillin.com>
2026-06-12 00:57:35 +0200

    Squash Prisma migrations

--- prisma/migrations/20260611225641_init/migration.sql
+++ prisma/migrations/20260611225641_init/migration.sql
@@ -13,8 +13,8 @@ CREATE TABLE "Review" (
     "rating" INTEGER NOT NULL,
     "comment" TEXT,
     "tags" TEXT[] DEFAULT ARRAY[]::TEXT[],
-    "posted" TIMESTAMP(3) NOT NULL,
-    "teaId" INTEGER NOT NULL,
+    "posted" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "teaId" TEXT,
     "userId" TEXT,
 
     CONSTRAINT "Review_pkey" PRIMARY KEY ("id")
@@ -43,6 +43,9 @@ CREATE TABLE "Suggestion" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Tea_name_key" ON "Tea"("name");
+
+-- AddForeignKey
+ALTER TABLE "Review" ADD CONSTRAINT "Review_teaId_fkey" FOREIGN KEY ("teaId") REFERENCES "Tea"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Review" ADD CONSTRAINT "Review_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
