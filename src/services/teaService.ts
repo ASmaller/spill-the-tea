dfa3e2b63c10df636f871a9c78de1065d77bf739
Daniel Kindvall <daniel.kindvall05@gmail.com>
2026-06-16 13:13:32 +0200

    fix format

--- src/services/teaService.ts
+++ src/services/teaService.ts
@@ -76,7 +76,9 @@ async function uploadImage(file: File, name: string): Promise<string> {
   const extension = IMAGE_EXTENSIONS[file.type];
 
   if (!extension) {
-    throw new Error("Unsupported image type. Supported types are: .jpg, .png, .webp, .gif, and .avif");
+    throw new Error(
+      "Unsupported image type. Supported types are: .jpg, .png, .webp, .gif, and .avif"
+    );
   }
 
   if (file.size <= 0) {
